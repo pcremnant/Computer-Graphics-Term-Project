a59b9156eecc1d657113eade2e3b80d3b8ce46fa
ParkChan <pcremnant@naver.com>
2019-11-26 00:07:07 +0900

    add object manager function - AddObject(), DeleteObject(), GetObject()

--- ComputerGraphics TermProject/ComputerGraphics TermProject/CObjectManager.h
+++ ComputerGraphics TermProject/ComputerGraphics TermProject/CObjectManager.h
@@ -8,7 +8,7 @@ private:
 	CCamera& camera;
 public:
 	CObjectManager(CCamera& cam) : camera(cam) {
-		vector_Objects.push_back(new CObject_cube(camera, glm::vec3{ 3,0.01,3 }, glm::vec3{ 0,-0.15,0 }));
+		//vector_Objects.push_back(new CObject_cube(camera, glm::vec3{ 3,0.01,3 }, glm::vec3{ 0,-0.15,0 }));
 	}
 
 	void Update() {
@@ -24,5 +24,21 @@ public:
 	void GetKeyboard(unsigned int key) {
 		for (auto iter : vector_Objects)
 			iter->GetKeyboard(key);
+	}
+
+	void AddObject(CObject* object) {
+		vector_Objects.push_back(object);
+	}
+
+	void DeleteObject(CObject* object) {
+		for (int i = 0; i < vector_Objects.size(); ++i) {
+			if (vector_Objects[i] == object) {
+				vector_Objects.erase(vector_Objects.begin() + i);
+			}
+		}
+	}
+
+	std::vector<CObject*> GetObjects() {
+		return vector_Objects;
 	}
 };
--- ComputerGraphics TermProject/ComputerGraphics TermProject/CScene_main.cpp
+++ ComputerGraphics TermProject/ComputerGraphics TermProject/CScene_main.cpp
@@ -2,6 +2,7 @@
 
 CScene_main::CScene_main() : CScene() {
 	pObjectManager = new CObjectManager(camera);
+	pObjectManager->AddObject(new CObject_cube(camera, glm::vec3{ 3,0.01,3 }, glm::vec3{ 0,-0.15,0 }));
 }
 void CScene_main::Update() {
 	pObjectManager->Update();
@@ -10,7 +11,16 @@ void CScene_main::Draw() {
 	pObjectManager->Draw();
 }
 void CScene_main::GetKeaboardInput(unsigned char key) {
+	// 테스트용 코드!
+	// 나중에 제대로 된 값으로 넣을 것
 	switch (key) {
+	case 't':
+		pObjectManager->AddObject(new CObject_cube(camera, glm::vec3{ 3,0.01,3 }, glm::vec3{ 0,-0.15,0 }));
+		break;
+	case 'd':
+		if (pObjectManager->GetObjects().size() != 0)
+			pObjectManager->DeleteObject(pObjectManager->GetObjects()[0]);
+		break;
 	case 'q':
 	case 'Q':
 		next = SetNextScene(FRAMEWORK_ACTION_POP, SCENE_TYPE_QUIT);
